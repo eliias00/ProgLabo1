321d8be1c8bafa31e02eaf73d23413019705ce3d
eliias00 <eliastronco@outlook.com>
2019-05-25 16:12:05 -0300

    ;)

--- parcial nuevo/informes.h
+++
@@ -1,7 +0,0 @@
-#ifndef INFORMES_H
-#define INFORMES_H
-
-void informeA(Orquesta *arrayOrq,int cantOrq);
-void informeB(Musico *arrayMus,int cantMus,Instrumento *arrayIns,Orquesta *arrayOrq,int cantOrq,int cantIns);
-
-#endif
